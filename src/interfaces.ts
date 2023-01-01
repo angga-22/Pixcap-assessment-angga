fff6630a48c3dccbd26c962bd8eadd0a70cfabd0
angga-22 <anggasp.unib@gmail.com>
2023-01-01 17:21:30 +0700

    feat: finishing assessment employee instances

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  uniqueId: number;
+  name: string;
+  subordinates: Employee[];
+}
+export interface IEmployeeOrgApp {
+  orgChart: Employee;
+  move(employeeID: number, supervisorID: number): void;
+  undo(): void;
+  redo(): void;
+}
